21ab8eb178345b23ad74ef4440e32333f4872378
Shaw Chifamba <shawchifamba@gmail.com>
2023-07-28 17:01:29 +0100

    Neater Solution

--- Transcribing DNA into RNA/RNA.py
+++ Transcribing DNA into RNA/RNA.py
@@ -1,10 +1,18 @@
+# Initial Attempt
+
+# with open ('rosalind_rna.txt', 'r') as f:
+#     DNA_input = f.read().strip()
+#     DNA = list(DNA_input)
+#     # replacing all occurrences of 'T' in t with 'U' in u.
+#     for i in range (0, len(DNA)):
+#         if DNA[i] == 'T':
+#             DNA[i] = 'U'
+#     RNA = DNA
+
+
+# Neater Solution using python str replace
 with open ('rosalind_rna.txt', 'r') as f:
-    DNA_input = f.read().strip()
-    DNA = list(DNA_input)
-    # replacing all occurrences of 'T' in t with 'U' in u.
-    for i in range (0, len(DNA)):
-        if DNA[i] == 'T':
-            DNA[i] = 'U'
-    RNA = DNA
-    print("".join(RNA))
-        
+    dna = f.read().strip()
+    rna = dna.replace('T', 'U')
+    
+    print(rna)
